c8d604666902f787bee6eebac56da39adb86bc25
Luna Hogart <moontochka@gmail.com>
2023-08-18 19:27:21 -0600

    final code

--- src/js/config.js
+++ src/js/config.js
@@ -4,3 +4,6 @@ export const TIMEOUT_SEC = 10;
 
 export const RES_PER_PAGE = 10;
 
+export const KEY = '97376e73-466a-43bf-afe1-50b8cb41352b';
+
+export const MODAL_CLOSE_SEC = 1.5;
--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -3,6 +3,7 @@ import View from './View.js';
 
 class AddRecipeView extends View {
     _parentElement = document.querySelector('.upload');
+    _message = 'Recipe was uploaded :)';
     _window = document.querySelector('.add-recipe-window');
     _overlay = document.querySelector('.overlay');
     _btnOpen = document.querySelector('.nav__btn--add-recipe');
@@ -28,14 +29,15 @@ class AddRecipeView extends View {
         this._overlay.addEventListener('click', this.toggleWindow.bind(this));
     };
 
+
     addHandlerUpload(handler) {
         this._parentElement.addEventListener('submit', function (e) {
             e.preventDefault();
             const dataArr = [...new FormData(this)];
             const data = Object.fromEntries(dataArr);
             handler(data);
-        })
-    }
+        });
+    };
 
     _generateMarkup() {
 
